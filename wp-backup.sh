d5d8b1d99f55db867124604255c16e2c2734f63c
Andros Fenollosa <tanrax@users.noreply.github.com>
2020-05-31 16:04:31 +0200

    Update wp-backup.sh

--- wp-backup.sh
+++ wp-backup.sh
@@ -6,36 +6,24 @@ FULL_PATH=$(pwd)
 BACKUP_PATH=$NOW
 
 #### Site-specific Info ####
-DB_NAME=`cat $SITE_PATH/wp-config.php | grep DB_NAME | cut -d \' -f 4`
-DB_USER=`cat $SITE_PATH/wp-config.php | grep DB_USER | cut -d \' -f 4`
-DB_PASS=`cat $SITE_PATH/wp-config.php | grep DB_PASSWORD | cut -d \' -f 4`
-DB_HOST=`cat $SITE_PATH/wp-config.php | grep DB_HOST | cut -d \' -f 4`
-
-#### Files Backup ####
-function files_backup {
-    zip -r wp-content.zip wp-content
-}
+DB_NAME=`cat wp-config.php | grep DB_NAME | cut -d \' -f 4`
+DB_USER=`cat wp-config.php | grep DB_USER | cut -d \' -f 4`
+DB_PASS=`cat wp-config.php | grep DB_PASSWORD | cut -d \' -f 4`
+DB_HOST=`cat wp-config.php | grep DB_HOST | cut -d \' -f 4`
 
 #### Database Backup ####
 function database_backup {
     mysqldump -h $DB_HOST -u$DB_USER -p$DB_PASS $DB_NAME > $DB_NAME.sql
 }
 
-#### Zip all Backups ####
-function pack_backups {
-    # Make folder
-    mkdir $BACKUP_PATH
-    # Move files to pack folder
-    mv wp-content.zip $BACKUP_PATH
-    # Move database to pack folder
-    mv $DB_NAME.sql $BACKUP_PATH
+#### Files Backup ####
+function files_backup {
     # Compress
-    zip -r $NOW.zip $BACKUP_PATH
-    # Remove temp folder
-    rm -r $BACKUP_PATH
+    zip -r $NOW.zip $BACKUP_PATH $DB_NAME.sql wp-content
 }
 
 #### Runner ####
-files_backup
 database_backup
-pack_backups
+files_backup
+
+#INSERT INTO `wp_options` VALUES (1,'siteurl','http://pixelmouse.es','yes'),(2,'home','http://pixelmouse.es','yes'),
