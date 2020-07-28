db5f55aa26f99e2c7bd8db7033f4fd97d86a6c0a
Andros Fenollosa <tanrax@users.noreply.github.com>
2020-07-28 09:46:25 +0200

    Update wp-backup.sh

--- wp-backup.sh
+++ wp-backup.sh
@@ -22,40 +22,29 @@ custom-sed() {
 export -f custom-sed
 
 #### VARIABLES ####
+
+# OS
 NAME_OSX="Darwin"
 THIS_OS=$(uname -mrs)
 PROGNAME=$(basename $0)
 NOW=$(date +"%Y-%m-%d-%H%M")
 FULL_PATH=$(pwd)
 BACKUP_PATH=$NOW
-NEW_SITE_URL='http://localhost'
-NEW_SITE_DOMAIN=$(custom-sed -nr "s/^https?:\/\/(.*)$/\1/p" $NEW_SITE_URL)
-SITE_URL=`cat $DB_NAME.sql | grep siteurl | cut -d \' -f 4`
-SITE_DOMAIN=$(custom-sed -nr "s/^https?:\/\/(.*)\/?/\1/p" $SITE_URL)
+
 # Site-specific Info
 DB_NAME=`cat wp-config.php | grep DB_NAME | cut -d \' -f 4`
 DB_USER=`cat wp-config.php | grep DB_USER | cut -d \' -f 4`
 DB_PASS=`cat wp-config.php | grep DB_PASSWORD | cut -d \' -f 4`
 DB_HOST=`cat wp-config.php | grep DB_HOST | cut -d \' -f 4`
+TABLE_PREFIX=$(cat wp-config.php | grep "\$table_prefix" | cut -d \' -f 2)
+
+# Domain
+NEW_SITE_URL='http://localhost'
+NEW_SITE_DOMAIN=$(custom-sed -nr "s/^https?:\/\/(.*)$/\1/p" $NEW_SITE_URL)
+SITE_URL=`mysql -e "SELECT option_value FROM $TABLE_PREFIX options WHERE option_name = 'siteurl'" -ss -u $DB_USER --password=$DB_PASS $DB_NAME`
+SITE_DOMAIN=$(custom-sed -nr "s/^https?:\/\/(.*)\/?/\1/p" $SITE_URL)
 
 #### FUNCTIONS ####
-
-# Create sed cross system
-custom-sed() {
-    if [[ $THIS_OS = *$NAME_OSX* ]]; then
-        # Check if OSX and install GSED
-        if [ -x "$(command -v gsed)" ]; then
-            gsed "$@"
-        else
-            echo "${COLOR_RED}ERROR. You must install gsed if you are using OSX${COLOR_RESET}"
-            exit 1
-        fi
-    else
-        # Linux
-        sed "$@"
-    fi
-}
-export -f custom-sed
 
 ## HELP
 usage() {
@@ -65,14 +54,14 @@ usage() {
 
     cat << EOF
 Usage: $PROGNAME [OPTION]
-Backup or restore a WordPress.
+Backup or restore a WordPress. Only Bash.
 
 Options:
-  backup-all                                    Create a zip file with important files and a copy of the database.
-  backup-database                               Backup of the database.
-  restore-all [{backup}.zip] [site url]         Restore a version.
-  restore-database [{database}.sql] [site url]  Restore the database.
-  --help                                        Display this usage message and exit
+  backup-all                         Create a zip file with important files and a copy of the database.
+  backup-database                    Backup of the database.
+  restore-all [{backup}.zip]         Restore a version.
+  restore-database [{database}.sql]  Restore the database.
+  --help                             Display this usage message and exit
 EOF
 
     exit 1
@@ -98,8 +87,10 @@ files_backup() {
 
 # Database Restore
 database_restore() {
-    # Replace `localhost` by SITE_URL
-    custom-sed -Ei "s,$NEW_SITE_URL,$SITE_URL,g" $1.sql
+    # Replace `http://localhost` by SITE_URL (`http://example.com`)
+    custom-sed -Ei "s,$NEW_SITE_URL,$SITE_URL,g" $1
+    # Replace `localhost` by SITE_DOMAIN (`example.com`): Fix multisite
+    custom-sed -Ei "s,$NEW_SITE_DOMAIN,$SITE_DOMAIN,g" $1
     # Restore tables
     cat $1 | /usr/bin/mysql -u DB_USER --password=DB_PASS DB_NAME
 }
